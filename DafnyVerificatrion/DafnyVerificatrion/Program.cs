5dcf9e065c58ff8ca2fc31e078a0e63f308d9bd9
Nemanja.Antic <nemanja.antic@enetelsolutions.com>
2019-05-19 19:54:33 +0200

    dafny compiled to cs file and imported to c# console app

---
+++ DafnyVerificatrion/DafnyVerificatrion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using _0_DafnyVerification_Compile;
+
+namespace DafnyVerificatrion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bubble Sort --------------------------");
+            System.Numerics.BigInteger[] numbers = { 2, 5, 7, 1, 5, 6, 18, 15, 16, 14 };
+            SortingService sortingService = new SortingService();
+            foreach(int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Sorted:");
+            sortingService.BubbleSort(numbers);
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Binary Search --------------------------");
+            BinarySearchService binarySearch = new BinarySearchService();
+            System.Numerics.BigInteger result;
+            binarySearch.BinarySearch(numbers, 7, out result);
+            Console.WriteLine(result);
+        }
+    }
+}
